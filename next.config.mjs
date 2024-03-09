1ea5306c95dc015ca2e3b92c1afd15ee77205527
Alexander Olier <aolierguerrero@gmail.com>
2024-03-09 10:26:31 -0500

    feat(dashboard): :art: configure nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    // start aplication in a subdomain before localhost
-    // subdomain: 'app',
-    async redirects() {
-      return [
-        {
-          source: '/',
-          destination: '/dashboard',
-          permanent: true,
-        },
-      ];
-    },
+  output: "export",
+  reactStrictMode: true,
+  swcMinify: true,
+  // start aplication in a subdomain before localhost
+  // subdomain: 'app',
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/dashboard",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
