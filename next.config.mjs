f9e41c39be8e141ceb21a45836eec958f29812d2
Alexander Olier <aolierguerrero@gmail.com>
2024-03-09 12:18:43 -0500

    fix(config-project): :bug: update dependencies & files

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: '/docs',
+  reactStrictMode: true,
+  swcMinify: true,
 };
-
 export default nextConfig;
