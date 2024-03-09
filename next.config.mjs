eba911405b1fda0736a9967706146996fff811da
Alexander Olier <aolierguerrero@gmail.com>
2024-03-09 11:04:22 -0500

    fix(config-project): :fire: update files

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  reactStrictMode: true,
   // start aplication in a subdomain before localhost
   // subdomain: 'app',
 };
 
-module.exports = nextConfig
+export default nextConfig;
