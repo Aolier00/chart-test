18c5da8603e4999cf4771085d5f35704e50ff98e
Alexander Olier <aolierguerrero@gmail.com>
2024-03-09 12:05:04 -0500

    fix(config-project): :zap: improvise

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  // start aplication in a subdomain before localhost
-  // subdomain: 'app',
+  basePath: '/docs',
 };
 
 export default nextConfig;
