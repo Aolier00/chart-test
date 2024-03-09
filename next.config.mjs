048461fb675f949c10841c27e5485d93e66bef5d
Alexander Olier <aolierguerrero@gmail.com>
2024-03-09 10:37:24 -0500

    fix(config-project): :fire: remove output nextjsconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  reactStrictMode: true,
-  swcMinify: true,
-  // start aplication in a subdomain before localhost
-  // subdomain: 'app',
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/dashboard",
-        permanent: true,
-      },
-    ];
-  },
+    reactStrictMode: true,
+    swcMinify: true,
+    // start aplication in a subdomain before localhost
+    // subdomain: 'app',
+    async redirects() {
+      return [
+        {
+          source: '/',
+          destination: '/dashboard',
+          permanent: true,
+        },
+      ];
+    },
 };
 
 export default nextConfig;
