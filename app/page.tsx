e0acb07dd49a43ce6fdef0782839e199849b1f59
Alexander Olier <aolierguerrero@gmail.com>
2024-03-09 10:46:37 -0500

    fix(config-project): :bug: add config static

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Home from "./dashboard/page";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  );
+};
+
+export default page;
--- next.config.mjs
+++ next.config.mjs
@@ -5,15 +5,6 @@ const nextConfig = {
   swcMinify: true,
   // start aplication in a subdomain before localhost
   // subdomain: 'app',
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/dashboard",
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
